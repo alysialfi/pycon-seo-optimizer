dd7072f44e629e86db57f9877810d5208b0f9865
9c.alysialfi <9c.alysialfi@gmail.com>
2024-10-26 04:09:24 +0000

    feat(analysis.py): add confition when meta not found & remove unused module

--- analysis.py
+++ analysis.py
@@ -1,14 +1,13 @@
 import requests
+import random
 from bs4 import BeautifulSoup
 from collections import defaultdict
 import re
 import pandas as pd
 from nlp_id.postag import PosTag
-import nltk
 from itertools import combinations
 import json
 
-nltk.download('punkt')
 postagger = PosTag()
 
 def analysis_result(data):
@@ -25,9 +24,16 @@ def analysis_result(data):
     }
 
     # Fetch the webpage
-    # print(sitename)
-    response = requests.get(sitename)
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.127 Safari/537.36',
+        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
+        'Accept-Language': 'en-US,en;q=0.5',
+        'Referer': sitename
+    }
+    response = requests.get(sitename, headers=headers)
     keywords_list = []
+
+    # return response
 
     if response.status_code == 200:
         soup = BeautifulSoup(response.text, 'html.parser')
@@ -38,10 +44,14 @@ def analysis_result(data):
             keywords_content = meta_keywords.get('content')
             keywords_list = [keyword.strip() for keyword in keywords_content.split(',')]
         else:
-            return []
+            match = re.search(r'/([^/]+)$', sitename)
+            if match:
+                route = match.group(1).replace("-", " ")
+                keywords_list = [route]
 
         body_text = soup.body.get_text(separator=' ', strip=True)
         res = postagger.get_phrase_tag(body_text)
+        # return res
         result_json = []
 
         category_count = defaultdict(lambda: defaultdict(int))
@@ -85,4 +95,10 @@ def analysis_result(data):
         final_json = json.dumps(result_json, indent=4)
         return final_json
     else:
-        print(f"Failed to retrieve the webpage. Status code: {response.status_code}")
+        result = {
+            "status": "failed",
+            "content": 'cannot request to targeted URL',  # This could be further processed text or data
+            "status_code": response.status_code
+        }
+        return result
+        # print(f"Failed to retrieve the webpage. Status code: {response.status_code}")
